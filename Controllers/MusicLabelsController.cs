89cff3cc2bdc917261c2dec40059b10f637f7208
s18580 <s18580@pjwstk.edu.pl>
2020-07-08 14:42:37 +0200

    Add controllers - MusicianController, MusicLabelController

---
+++ Controllers/MusicLabelsController.cs
@@ -0,0 +1,31 @@
+﻿    using Microsoft.AspNetCore.Mvc;
+using SampleAPI.Exceptions;
+using SampleAPI.Services;
+
+namespace SampleAPI.Controllers
+{
+    [Route("api/music-labels")]
+    [ApiController]
+    public class MusicLabelsController : ControllerBase
+    {
+        private readonly IMusicDbService _dbService;
+
+        public MusicLabelsController(IMusicDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetMusicLabel(int id)
+        {
+            try { 
+                var res = _dbService.GetMusicLabel(id);
+                return Ok(res);
+            }
+            catch (MusicLabelDoNotExistsException exc)
+            {
+                return NotFound(exc.Message);
+            }       
+        }
+    }
+}
---
+++ Controllers/MusiciansController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleAPI.Exceptions;
+using SampleAPI.Services;
+
+namespace SampleAPI.Controllers
+{
+    [Route("api/musicians")]
+    [ApiController]
+    public class MusiciansController : ControllerBase
+    {
+
+        private readonly IMusicDbService _dbService;
+
+        public MusiciansController(IMusicDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult RemoveMusician(int id)
+        {
+            try
+            {
+                _dbService.RemoveMusician(id);
+                return NoContent();
+            }
+            catch (MusicianDoesNotExistsException exc)
+            {
+                return NotFound(exc.Message);
+            }
+        }
+    }
+}
+    
